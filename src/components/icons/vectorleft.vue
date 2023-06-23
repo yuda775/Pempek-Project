240d6d2e7328fc9a34edb978fdd361107b8f970e
alzannajmir <alzannajmiramadhan@gmail.com>
2023-06-23 11:22:58 +0700

    pempek1.0

--- src/components/icons/vectorleft.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <svg width="410" height="300" viewBox="0 0 410 300" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 50C0 22.3858 22.3858 0 50 0H410V250C410 277.614 387.614 300 360 300H0V50Z" fill="#FF9F0D"/>
-        </svg>
-    </div>
-</template>
--- src/components/icons/vectorright.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <svg width="415" height="300" viewBox="0 0 415 300" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0H365C392.614 0 415 22.3858 415 50V300H50C22.3858 300 0 277.614 0 250V0Z" fill="#FF9F0D"/>
-        </svg>
-    </div>
-</template>
